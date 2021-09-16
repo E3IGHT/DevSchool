52495add304ce7b482e7b82edaabd2c1ae899d51
Mariana <ra36816458842@acaonsfatima.org.br>
2021-09-16 19:04:25 -0300

    ok

--- api/src/index.js
+++ api/src/index.js
@@ -74,6 +74,11 @@ app.put('/produto/:id', async (req,resp) => {
                || descricao == '' || estoque == '' || imagem == '' ) {
                    return resp.send( { erro: 'Todos os campos são obrigatórios.'} )
         }
+        
+        if(nome == null || categoria == null || precode == null || precopor == null || avaliacao == null
+            || descricao == null || estoque == null || imagem == null ) {
+                return resp.send( { erro: 'Todos os campos são obrigatórios.'} )
+            }
 
         if(isNaN(estoque) || isNaN(precode) || isNaN(precopor) || isNaN(avaliacao)) {
             return resp.send( { erro: 'Os campos estoque, preço de, preço por e avaliação precisam ser campos numéricos.'} )
@@ -86,7 +91,7 @@ app.put('/produto/:id', async (req,resp) => {
         let d = await db.tb_produto.findOne( { where: { nm_produto: nome } } );
         
         if( d != null ) {
-            if( d.id_produto == a )
+            if( d.id_produto != a )
             return resp.send( {erro: 'Não podem existir produtos com o mesmo nome.'} )
         }
 
